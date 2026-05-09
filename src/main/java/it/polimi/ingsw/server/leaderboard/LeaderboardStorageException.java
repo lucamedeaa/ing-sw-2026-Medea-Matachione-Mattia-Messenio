3161b1dcbce49a60e1f3ca21932e9987d74f1082
Edoardo Matachione <edoardomatachione@gmail.com>
2026-05-09 17:29:00 +0200

    Added DB

---
+++ src/main/java/it/polimi/ingsw/server/leaderboard/LeaderboardStorageException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.server.leaderboard;
+
+public class LeaderboardStorageException extends RuntimeException {
+    public LeaderboardStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
